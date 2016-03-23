f48d3ca47cc994243cd0938ebb3694f5dbee2802
Maël Valais <mael.valais@gmail.com>
2016-03-23 13:15:05 +0100

    Better handling on already symlinked dotfiles

--- installdotfiles.sh
+++ installdotfiles.sh
@@ -14,24 +14,50 @@
 if [ ! -d "old_dotfiles" ]; 
     then mkdir old_dotfiles; 
 fi
-
-ls -p -a | grep -v / | grep '^\.' | while read dotfile;  do 
+already=""
+# The command 
+#     find ~ -maxdepth 1 ! -type l ! -type d 
+# lists every normal file (no directories, no symlinks)
+find . -maxdepth 1 ! -type l ! -type d -iname ".*" | while read dotfile;  do 
+    dotfile=`basename "$dotfile"`
     if [ "$dotfile" != ".DS_Store" ]; then
         found=0
-        ls -p -a ~ | grep -v / | grep '^\.' | while read dotfile_in_home; do
+        find ~ -maxdepth 1 ! -type l ! -type d -iname ".*" | while read dotfile_in_home; do
+            dotfile_in_home=`basename "$dotfile_in_home"`
             if [ "$dotfile" == "$dotfile_in_home" ]; then
-                echo "Moving \~/$dotfile to old_dotfiles and symlinking instead with $dotfile"
+                echo "Moving $dotfile_in_home to old_dotfiles and symlinking instead with $dotfile"
                 mv ~/$dotfile_in_home "./old_dotfiles/"
                 ln -s "$dotfile" "~/$dotfile"
                 found=1
             fi
         done
-        if [ $found -eq 1 ]; then
-            echo "No \~/$dotfile. Directly symlinking."            
-            ln -s "$dotfile" "~/$dotfile"            
+        if [ $found -eq 0 ]; then
+            # Check if this .file has already a symlink in ~
+            if (find ~/$dotfile -maxdepth 1 -type l 2>/dev/null >/dev/null); then
+                already="$already $dotfile_in_home"
+            else
+                echo "No ~/$dotfile. Directly symlinking."
+            fi
+            ln -s "$dotfile" "~/$dotfile" 
         fi
     fi
 done
+if [ "x$already" != "x" ]; then
+    echo $already | while read dotfile; do
+        echo "$dotfile" already has a symlink in home.
+    done
+fi
+
+# find . -type t
+# b       block special
+# c       character special
+# d       directory
+# f       regular file
+# l       symbolic link
+# p       FIFO
+# s       socket
+
+
 
 # for file in $files; do
 #     echo "Moving any existing dotfiles from ~ to $olddir"
