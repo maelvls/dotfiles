c6abd20fdc6b571ed8f13ec08304fc4dea7b4478
Maël Valais <mael.valais@gmail.com>
2016-03-23 12:41:54 +0100

    Fix script

--- installdotfiles.sh
+++ installdotfiles.sh
@@ -1,10 +1,41 @@
 #! /bin/bash
 #
-# install_dot_files.sh
-# Copyright (C) 2016 Maël Valais <mael.valais@gmail.com>
+# installdotfiles.sh
+# Maël Valais
 #
+# This script creates symlinks to every .files in the current directory
+# into the ~/ directory. Overwritten ~/.files will be stored into old_dotfiles/
+#
+# Note: Remember to run "chmod +x installdotfile.sh" if any issues occur
+#
+# vim:set et sts=0 sw=4 ts=4:
 #
 
-# ln -s original symbolic
+if [ ! -d "old_dotfiles" ]; 
+    then mkdir old_dotfiles; 
+fi
 
-ln -s .vimrc ~/.vimrc
+ls -p -a | grep -v / | grep '^\.' | while read dotfile;  do 
+    if [ "$dotfile" != ".DS_Store" ]; then
+        found=0
+        ls -p -a ~ | grep -v / | grep '^\.' | while read dotfile_in_home; do
+            if [ "$dotfile" == "$dotfile_in_home" ]; then
+                echo "Moving \~/$dotfile to old_dotfiles and symlinking instead with $dotfile"
+                mv ~/$dotfile_in_home "./old_dotfiles/"
+                ln -s "$dotfile" "~/$dotfile"
+                found=1
+            fi
+        done
+        if [ $found -eq 1 ]; then
+            echo "No \~/$dotfile. Directly symlinking."            
+            ln -s "$dotfile" "~/$dotfile"            
+        fi
+    fi
+done
+
+# for file in $files; do
+#     echo "Moving any existing dotfiles from ~ to $olddir"
+#     mv ~/.$file ~/dotfiles_old/
+#     echo "Creating symlink to $file in home directory."
+#     ln -s $dir/$file ~/.$file
+# done
