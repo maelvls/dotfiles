d324a537c04e6c887d1d16bc3bf16e2ffe50787e
Maël Valais <mael.valais@gmail.com>
2016-10-20 18:16:22 +0200

    Enhanced installdotfiles.sh with < <() (named pipes)

--- installdotfiles.sh
+++ installdotfiles.sh
@@ -4,7 +4,7 @@
 # Maël Valais
 #
 # This script creates symlinks to every .files in the current directory
-# into the ~/ directory. Overwritten ~/.files will be stored into old_dotfiles/
+# into the $HOME/ directory. Overwritten $HOME/.files will be stored into old_dotfiles/
 #
 # Note: Remember to run "chmod +x installdotfile.sh" if any issues occur
 #
@@ -16,41 +16,42 @@ if [ ! -d "old_dotfiles" ];
 fi
 already=""
 # The command 
-#     find ~ -maxdepth 1 ! -type l ! -type d 
+#     find $HOME -maxdepth 1 ! -type l ! -type d 
 # lists every normal file (no directories, no symlinks)
 while read dotfile;  do 
-    dotfile=`basename "$dotfile"`
-    if [ "$dotfile" != ".DS_Store" ]; then
-        found=0
-        while read dotfile_in_home; do
-            dotfile_in_home=`basename "$dotfile_in_home"`
-            if [ "$dotfile" == "$dotfile_in_home" ]; then
-                echo "Moving $dotfile_in_home to old_dotfiles and symlinking instead with $dotfile"
-                mv ~/$dotfile_in_home "old_dotfiles/"
-                ln -s `pwd`/"$dotfile" ~/"$dotfile"
-                found=1
-            fi
-        done <<< $(find ~ -maxdepth 1 ! -type l ! -type d -iname ".*")
-        if [ $found -eq 0 ]; then
-            # Check if this .file has already a symlink in ~
-            if (find ~/$dotfile -maxdepth 1 -type l 2>/dev/null >/dev/null); then
-                already=$dotfile $already
-                echo "$dotfile" already has a symlink in home.
-            else
-                echo "No ~/$dotfile. Directly symlinking."
-                ln -s `pwd`/"$dotfile" ~/"$dotfile"
-            fi
-             
+    found=0
+    while read dotfile_in_home; do
+        if [ "$dotfile" == "$dotfile_in_home" ]; then
+            echo "Moving $dotfile_in_home to old_dotfiles and symlinking instead with $dotfile"
+            echo mv $HOME/$dotfile_in_home "old_dotfiles/"
+	    mv $HOME/$dotfile_in_home "old_dotfiles/"
+	    echo ln -s `pwd`/"$dotfile" $HOME/"$dotfile"
+            ln -s `pwd`/"$dotfile" $HOME/"$dotfile"
+            found=1
         fi
+done < <(find $HOME -maxdepth 1 ! -type l ! -type d \
+	\( -name ".*" -a ! -name ".DS_Store" \) -exec basename {} \;)
+    if [ $found -eq 0 ]; then
+        # Check if this .file has already a symlink in $HOME
+        if (find "$HOME/$dotfile" -maxdepth 1 -type l 2>/dev/null >/dev/null); then
+            already="${dotfile} ${already}"
+            echo "$dotfile" already has a symlink in home.
+        else
+            echo "No $HOME/$dotfile. Directly symlinking."
+            echo ln -s "$PWD/$dotfile" "$HOME/$dotfile"
+	    ln -s "$PWD/$dotfile" "$HOME/$dotfile"
+        fi
+         
     fi
-done <<< $(find . -maxdepth 1 ! -type l ! -type d -iname ".*")
+done < <(find . -maxdepth 1 ! -type l ! -type d \
+	   \( -name ".*" -a ! -name ".DS_Store" \) -exec basename {} \;)
 
 if [ "$already" -a "$1" == "-f" ]; then
-    while read dotfile; do 
-        rm ~/"$dotfile"
-        ln -s `pwd`/"$dotfile" ~/"$dotfile"
-        echo "Replaced ~/$dotfile symlink."
-    done <<< $(echo "$already")
+    for dotfile in `echo $already`; do 
+        rm "$HOME/$dotfile"
+        ln -s "$PWD/$dotfile" "$HOME/$dotfile"
+        echo "Replaced $HOME/$dotfile symlink."
+    done
 else 
     echo "The following files already exist in home:"
     echo $already
@@ -68,8 +69,8 @@ fi
 
 
 # for file in $files; do
-#     echo "Moving any existing dotfiles from ~ to $olddir"
-#     mv ~/.$file ~/dotfiles_old/
+#     echo "Moving any existing dotfiles from $HOME to $olddir"
+#     mv $HOME/.$file $HOME/dotfiles_old/
 #     echo "Creating symlink to $file in home directory."
-#     ln -s $dir/$file ~/.$file
+#     ln -s $dir/$file $HOME/.$file
 # done
