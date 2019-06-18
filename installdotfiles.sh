f226afb671e58454d70cc8c8718e1b360162c808
Maël Valais <mael.valais@gmail.com>
2019-06-18 13:08:47 +0200

    add colors, add gpakosz-tmux

--- installdotfiles.sh
+++ installdotfiles.sh
@@ -11,65 +11,89 @@
 # vim:set et sts=0 sw=4 ts=4:
 #
 
+gray='\033[90m'
+red='\033[91m'
+green='\033[92m'
+yel='\033[93m'
+# blu='\033[94m'
+end='\033[0m'
+function trace() {
+    echo -ne "$1 ${gray}"
+    LANG=C perl -e 'print join (" ", map { $_ =~ / / ? "\"".$_."\"" : $_} @ARGV)' -- "${@:2}"
+    echo -e "${end}"
+    command "$@"
+}
+
+HOME_DOTFILES=$(find "$HOME" -maxdepth 1 ! -type l ! -type d \( -name ".*" -a ! -name ".DS_Store" \) -exec basename {} \;)
+PWD_DOTFILES=$(find . -maxdepth 1 ! -type l ! -type d \( -name ".*" -a ! -name ".DS_Store" \) -exec basename {} \;)
+
+# Install Vim-plug for Vim 8
+[ -f ~/.vim/autoload/plug.vim ] || curl -sfLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install gpakosz-tmux: https://github.com/gpakosz/.tmux.git
+# My personal conf is in .tmux.conf.local
+git submodule update
+PWD_DOTFILES+=$'\n'gpakosz-tmux/.tmux.conf
+
+FORCE=no
+BREW=no
 while [ $# -gt 0 ]; do
-	case "$FORCE" in
-		--force) FORCE=yes ;;
-		*) ;;
-	esac
-	shift
+    case "$1" in
+    --force | -f) FORCE=yes ;;
+    --brew) BREW=yes ;;
+    *) ;;
+    esac
+    shift
 done
 
-if [ ! -d "old_dotfiles" ]; 
-    then mkdir old_dotfiles; 
+if [ ! -d "old_dotfiles" ]; then
+    mkdir old_dotfiles
 fi
 
-already=""
-# The command 
-#     find $HOME -maxdepth 1 ! -type l ! -type d 
+ALREADY=""
+# The command
+#     find $HOME -maxdepth 1 ! -type l ! -type d
 # lists every normal file (no directories, no symlinks)
-while read dotfile;  do 
+for dotfile in $PWD_DOTFILES; do
+    SOURCE="$PWD/$dotfile"
+    TARGET="$HOME/$(basename "$dotfile")"
     found=0
-    while read dotfile_in_home; do
-        if [ "$dotfile" == "$dotfile_in_home" ]; then
-            echo "Moving $dotfile_in_home to old_dotfiles and symlinking instead with $dotfile"
-            echo mv $HOME/$dotfile_in_home "old_dotfiles/"
-	    mv $HOME/$dotfile_in_home "old_dotfiles/"
-	    echo ln -s `pwd`/"$dotfile" $HOME/"$dotfile"
-            ln -s `pwd`/"$dotfile" $HOME/"$dotfile"
+    for dotfile_in_home in $HOME_DOTFILES; do
+        if [ "$TARGET" == "$dotfile_in_home" ]; then
+            echo "Moving ${yel}$dotfile_in_home${end} to ${yel}./old_dotfiles${end} and symlinking instead with ${yel}$dotfile${end}"
+            trace mv "$HOME/$dotfile_in_home" "old_dotfiles/"
+            trace ln -s "$SOURCE" "$TARGET" # source is in PWD, target is in HOME
             found=1
         fi
-done < <(find $HOME -maxdepth 1 ! -type l ! -type d \
-	\( -name ".*" -a ! -name ".DS_Store" \) -exec basename {} \;)
+    done < <(echo "$HOME_DOTFILES")
     if [ $found -eq 0 ]; then
         # Check if this .file has already a symlink in $HOME
-        if (find "$HOME/$dotfile" -maxdepth 1 -type l 2>/dev/null >/dev/null); then
-		if [ "$FORCE" = "yes" ]; then
-            		echo "$dotfile" already has a symlink in home.
-		else
-			echo "(--force) $dotfile already exists, resymlinking."
-            		echo ln -sf "$PWD/$dotfile" "$HOME/$dotfile"
-	    		ln -sf "$PWD/$dotfile" "$HOME/$dotfile"
-		fi
+        if (find "$TARGET" -maxdepth 1 -type l 2>/dev/null >/dev/null); then
+            if [ "$FORCE" = "no" ]; then
+                echo -e "${yel}$dotfile${end} already has a symlink in home. To force resymlink, use ${red}--force${end}"
+                ALREADY+="\n$dotfile"
+            else
+                echo -e "(${red}--force${end} mode) ${yel}$dotfile${end} already exists, resymlinking."
+                trace ln -sf "$SOURCE" "$TARGET"
+            fi
         else
-            echo "No $HOME/$dotfile. Directly symlinking."
-            echo ln -s "$PWD/$dotfile" "$HOME/$dotfile"
-	    ln -s "$PWD/$dotfile" "$HOME/$dotfile"
+            echo -e "No ${yel}$HOME/$dotfile${end}. Directly symlinking."
+            trace ln -s "$SOURCE" "$TARGET"
         fi
-         
-    fi
-done < <(find . -maxdepth 1 ! -type l ! -type d \
-	   \( -name ".*" -a ! -name ".DS_Store" \) -exec basename {} \;)
 
-if [ "$already" -a "$1" == "-f" ]; then
-    for dotfile in `echo $already`; do 
+    fi
+done
+
+if [ -n "$ALREADY" ] && [ "$1" == "-f" ]; then
+    for dotfile in $ALREADY; do
         rm "$HOME/$dotfile"
         ln -s "$PWD/$dotfile" "$HOME/$dotfile"
         echo "Replaced $HOME/$dotfile symlink."
     done
-else 
-    echo "The following files already exist in home:"
-    echo $already
-    echo $0 -f to force symlink
+elif [ -n "$ALREADY" ]; then
+    echo -e "${red}The following files already exist in home:${end}"
+    echo -e "${green}$ALREADY${end}"
+    echo -e "You can use '${green}$0 --force${end}' to force symlink"
 fi
 # find . -type t
 # b       block special
@@ -80,8 +104,6 @@ fi
 # p       FIFO
 # s       socket
 
-
-
 # for file in $files; do
 #     echo "Moving any existing dotfiles from $HOME to $olddir"
 #     mv $HOME/.$file $HOME/dotfiles_old/
@@ -89,17 +111,17 @@ fi
 #     ln -s $dir/$file $HOME/.$file
 # done
 
-
-
-# Install Vundle for Vim
-[ -d "~/.vim/bundle/Vundle.vim" ] || git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
 # Install tpm, the tmux package manager
-[ -d "~/.tmux/plugins/tpm" ] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-[ -d "~/.tmux/plugins/gpakosz-tmux-conf" ] || (git clone https://github.com/gpakosz/.tmux.git ~/.tmux/plugins/gpakosz-tmux-conf && ln -s -f ~/.tmux.conf ~/.tmux/plugins/gpakosz-tmux-conf/.tmux.conf)
+[ -d "$HOME/.tmux" ] || mkdir "$HOME/.tmux"
+[ -d "$HOME/.tmux/plugins/tpm" ] || git clone -q https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+[ -d "$HOME/.tmux/plugins/gpakosz-tmux-conf" ] || (git clone -q https://github.com/gpakosz/.tmux.git ~/.tmux/plugins/gpakosz-tmux-conf && ln -s -f ~/.tmux.conf ~/.tmux/plugins/gpakosz-tmux-conf/.tmux.conf)
 
 # Install brew if not installed
-if ! [ -d /usr/local/Cellar -o -d "$HOME/.linuxbrew" -o -d "/home/linuxbrew/.linuxbrew" ]; then
+if [ "$BREW" != yes ]; then
+    exit 0
+fi
+
+if ! [ -d /usr/local/Cellar -o -d "$HOME/.linuxbrew" ] || [ -d "/home/linuxbrew/.linuxbrew" ]; then
     if [ "$(uname -s)" = Darwin ]; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     else
@@ -108,8 +130,7 @@ if ! [ -d /usr/local/Cellar -o -d "$HOME/.linuxbrew" -o -d "/home/linuxbrew/.lin
 fi
 
 BREWS=/tmp/brews
-cat > $BREWS <<EOF
-    ack
+cat >$BREWS <<EOF
     coreutils
     curl
     diff-so-fancy
@@ -127,14 +148,14 @@ cat > $BREWS <<EOF
 EOF
 # mac-only packages
 if [ "$(uname -s)" = Darwin ]; then
-cat >> $BREWS <<EOF
+    cat >>$BREWS <<EOF
     reattach-to-user-namespace
     pinentry-mac
 EOF
 fi
 
 # Install the brew packages
-if which brew >/dev/null 2>&1; then
-    brew install $(cat $BREWS)
+if command -v brew >/dev/null 2>&1; then
+    brew install $BREWS
 fi
 rm -f $BREWS
